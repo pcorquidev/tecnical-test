bd74d1b74d7e10524ffa86b40df267743b380db2
Paul Cortes <pcorqui@gmail.com>
2024-05-30 19:01:10 -0600

    feat(repository): se agrega repository MarvelServiceRepository

---
+++ src/main/java/io/bootify/tecnical_test/repository/MarvelServiceRepository.java
@@ -0,0 +1,8 @@
+package io.bootify.tecnical_test.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MarvelServiceRepository extends JpaRepository {
+}
