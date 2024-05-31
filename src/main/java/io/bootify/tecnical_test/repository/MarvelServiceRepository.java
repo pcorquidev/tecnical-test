ba1e10c55bacc6afb50f36a082a33b3e69204d09
Paul Cortes <pcorqui@gmail.com>
2024-05-30 19:15:33 -0600

    fix(repository): se agrega el tipo para el repository

--- src/main/java/io/bootify/tecnical_test/repository/MarvelServiceRepository.java
+++ src/main/java/io/bootify/tecnical_test/repository/MarvelServiceRepository.java
@@ -1,8 +1,9 @@
 package io.bootify.tecnical_test.repository;
 
+import io.bootify.tecnical_test.domain.MarvelServiceQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MarvelServiceRepository extends JpaRepository {
+public interface MarvelServiceRepository extends JpaRepository<MarvelServiceQuery,Long> {
 }
